61be8e16670b6a8cf344bf63679f9affac3a5232
Mai Pham Quang Huy <maipham.quanghuy@gmail.com>
2017-12-28 16:07:28 +0700

    Update app.js

--- app.js
+++ app.js
@@ -3,13 +3,14 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var mongo = require('mongodb');
 var monk = require('monk');
+var http = require('http').Server(app);
  
 //create neccessary objects
 var app = express();
 var router = express.Router();
  
 //you need to update wp with your own database name
-var db = monk('mongodb://imhikarucat:12345abcde@ds157444.mlab.com:57444/a2-webpro-s3-2017');
+var db = monk('mongodb://DB_Username:DB_Password@DB_URL'); //db name here
  
  
 //use objects in app
@@ -20,7 +21,7 @@ app.use(function(req,res,next){
 	req.db = db;
 	next();
 });
- 
+
 //CLIENT SIDE ROUTING
 app.set('views', __dirname+'/views');
  
@@ -30,32 +31,35 @@ app.get('/home', function(req, res){
 });
  
  
- 
 //SERVER SIDE ROUTING
 app.use('/', router);
 
-//STUDENT
+//Get All
 router.get('/students', function(req, res) {
     req.db.collection('students').find({},{"limit": 100},function(e,docs){
     	res.json(docs);
 	});
 });
  
-router.get('/student/:id', function(req, res){
-  	req.db.collection('students').findById(req.params.id, function(e, doc){
-        	res.json(doc);
-  	})
+//Get By _id
+router.get('/students/:id', function(req, res){
+	req.db.collection('students').findOne(req.params.id, function(e, doc){
+		  res.json(doc);
+	})
 });
+
  
-router.put('/student/:id', function(req, res){
+//Update by _id
+router.put('/students/:id', function(req, res){
   	req.db.collection('students').update({_id: req.params.id}, {name: req.body.name, yob: req.body.yob});
-  	req.db.collection('students').findById(req.params.id, function(e, doc){
+  	req.db.collection('students').findOne(req.params.id, function(e, doc){
         	res.json(doc);
   	})
  
 });
- 
-router.delete('/student/:id', function(req, res){
+
+//Delete by _id
+router.delete('/students/:id', function(req, res){
   	req.db.collection('students').remove({_id: req.params.id}, function(e, doc){
         	res.json(doc);
   	})
@@ -68,8 +72,8 @@ router.post('/students', function(req, res){
         	res.json(docs);
   	});
 });
- 
-//PRODUCTS
+
+//PRODUCT
 router.get('/products', function(req, res) {
     req.db.collection('products').find({},{"limit": 100},function(e,docs){
     	res.json(docs);
@@ -77,23 +81,22 @@ router.get('/products', function(req, res) {
 });
  
 router.get('/products/:id', function(req, res){
-  	req.db.collection('products').findById(req.params.id, function(e, doc){
+  	req.db.collection('products').findOne(req.params.id, function(e, doc){
         	res.json(doc);
   	})
 });
  
 router.put('/products/:id', function(req, res){
-	  req.db.collection('products').update({_id: req.params.id}, 
-		{
-			id: req.body.id, 
-			name: req.body.name, 
-			price: req.body.price, 
-			description: req.body.description, 
-			brand: req.body.brand, 
-			producer: req.body.producer, 
-			inventory: req.body.inventory, 
-			imageurl: req.body.imageurl});
-  	req.db.collection('products').findById(req.params.id, function(e, doc){
+  	req.db.collection('products').update({_id: req.params.id}, {
+		  id: req.body.id,
+          name: req.body.name, 
+          price: req.body.price, 
+          description: req.body.description, 
+          brand: req.body.brand, 
+          producer: req.body.producer,
+          imageurl: req.body.imageurl,
+        });
+  	req.db.collection('products').findOne(req.params.id, function(e, doc){
         	res.json(doc);
   	})
  
@@ -106,14 +109,13 @@ router.delete('/products/:id', function(req, res){
 });
  
 router.post('/products', function(req, res){
- 
   	console.log(req.body);
   	req.db.collection('products').insert(req.body, function(e, docs){
         	res.json(docs);
   	});
 });
 
-//CATEGORIES
+//PRODUCT CATEGORIES == PRODUCT TYPES???
 router.get('/categories', function(req, res) {
     req.db.collection('categories').find({},{"limit": 100},function(e,docs){
     	res.json(docs);
@@ -121,18 +123,17 @@ router.get('/categories', function(req, res) {
 });
  
 router.get('/categories/:id', function(req, res){
-  	req.db.collection('categories').findById(req.params.id, function(e, doc){
+  	req.db.collection('categories').findOne(req.params.id, function(e, doc){
         	res.json(doc);
   	})
 });
  
 router.put('/categories/:id', function(req, res){
-	  req.db.collection('categories').update({_id: req.params.id}, 
-		{
-			id: req.body.id,
-			name: req.body.name,	
-		});
-  	req.db.collection('categories').findById(req.params.id, function(e, doc){
+  	req.db.collection('categories').update({_id: req.params.id}, {
+		  id: req.body.id,
+          name: req.body.name,
+        });
+  	req.db.collection('categories').findOne(req.params.id, function(e, doc){
         	res.json(doc);
   	})
  
@@ -145,53 +146,13 @@ router.delete('/categories/:id', function(req, res){
 });
  
 router.post('/categories', function(req, res){
- 
   	console.log(req.body);
   	req.db.collection('categories').insert(req.body, function(e, docs){
         	res.json(docs);
   	});
 });
 
-//SHOOPING CARTS
-router.get('/shoppingCarts', function(req, res) {
-    req.db.collection('shoppingCarts').find({},{"limit": 100},function(e,docs){
-    	res.json(docs);
-	});
-});
- 
-router.get('/shoppingCarts/:id', function(req, res){
-  	req.db.collection('shoppingCarts').findById(req.params.id, function(e, doc){
-        	res.json(doc);
-  	})
-});
- 
-router.put('/shoppingCarts/:id', function(req, res){
-	  req.db.collection('shoppingCarts').update({_id: req.params.id}, 
-		{
-			id: req.body.id,
-			name: req.body.name,	
-		});
-  	req.db.collection('shoppingCarts').findById(req.params.id, function(e, doc){
-        	res.json(doc);
-  	})
- 
-});
- 
-router.delete('/shoppingCarts/:id', function(req, res){
-  	req.db.collection('shoppingCarts').remove({_id: req.params.id}, function(e, doc){
-        	res.json(doc);
-  	})
-});
- 
-router.post('/shoppingCarts', function(req, res){
- 
-  	console.log(req.body);
-  	req.db.collection('shoppingCarts').insert(req.body, function(e, docs){
-        	res.json(docs);
-  	});
-});
-
-//ORDERS
+//ORDER
 router.get('/orders', function(req, res) {
     req.db.collection('orders').find({},{"limit": 100},function(e,docs){
     	res.json(docs);
@@ -199,18 +160,22 @@ router.get('/orders', function(req, res) {
 });
  
 router.get('/orders/:id', function(req, res){
-  	req.db.collection('orders').findById(req.params.id, function(e, doc){
+  	req.db.collection('orders').findOne(req.params.id, function(e, doc){
         	res.json(doc);
   	})
 });
  
 router.put('/orders/:id', function(req, res){
-	  req.db.collection('orders').update({_id: req.params.id}, 
-		{
-			id: req.body.id,
-			name: req.body.name,	
-		});
-  	req.db.collection('orders').findById(req.params.id, function(e, doc){
+  	req.db.collection('orders').update({_id: req.params.id}, {
+		  id: req.body.id,
+          name: req.body.name, 
+		  email: req.body.email,
+		  phone: req.body.phone,
+		  address: req.body.address,
+		  orderdate: req.body.orderdate,
+		  total: req.body.total,
+        });
+  	req.db.collection('orders').findOne(req.params.id, function(e, doc){
         	res.json(doc);
   	})
  
@@ -223,12 +188,58 @@ router.delete('/orders/:id', function(req, res){
 });
  
 router.post('/orders', function(req, res){
- 
   	console.log(req.body);
   	req.db.collection('orders').insert(req.body, function(e, docs){
         	res.json(docs);
   	});
 });
+
+//SHOPPING CART
+router.get('/shoppingCarts', function(req, res) {
+    req.db.collection('shoppingCarts').find({},{"limit": 100},function(e,docs){
+    	res.json(docs);
+	});
+});
+ 
+router.get('/shoppingCarts/:id', function(req, res){
+  	req.db.collection('shoppingCarts').findOne(req.params.id, function(e, doc){
+        	res.json(doc);
+  	})
+});
+ 
+router.put('/shoppingCarts/:id', function(req, res){
+  	req.db.collection('shoppingCarts').update({_id: req.params.id}, {
+		  id: req.body.name,
+          name: req.body.name,
+        });
+  	req.db.collection('shoppingCarts').findOne(req.params.id, function(e, doc){
+        	res.json(doc);
+  	})
+ 
+});
+ 
+router.delete('/shoppingCarts/:id', function(req, res){
+  	req.db.collection('shoppingCarts').remove({_id: req.params.id}, function(e, doc){
+        	res.json(doc);
+  	})
+});
+ 
+router.post('/shoppingCarts', function(req, res){
+  	console.log(req.body);
+  	req.db.collection('shoppingCarts').insert(req.body, function(e, docs){
+        	res.json(docs);
+  	});
+});
+
+module.exports = app;
+// app.listen(8080);
+// http.listen(process.env.PORT || 8080, function(){
+//     console.log('listening on', http.address().port);
+//   });
+
+// var server = app.listen(8080, function(){
+//     console.log('Ready on port %d', server.address().port);
+// });
 
 app.set( 'port', ( process.env.PORT || 8080 ));
 
