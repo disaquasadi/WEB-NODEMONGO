862cd83659d05f7ca8985f5173f0f1525de62831
Mai Pham Quang Huy <maipham.quanghuy@gmail.com>
2017-12-30 11:24:15 +0700

    Update app.js

--- app.js
+++ app.js
@@ -20,7 +20,9 @@ var router = express.Router();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(function (req, res) {
-    res.setHeader('Content-Type', 'application/json');
+	res.setHeader('Content-Type', 'application/json');
+	res.write('you posted:\n');
+  	res.end(JSON.stringify(req.body, null, 2));
     // res.send(JSON.stringify({}));
   })
 
